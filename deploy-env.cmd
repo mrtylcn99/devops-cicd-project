d68df955ca953be6d27574860acf8536429feee4
mrtylcn99 <mrtylcn99@users.noreply.github.com>
2026-01-18 04:42:36 +0300

    feat: Add static admin password for Argo CD
    - Admin password set to: DevOps2024!
    - Password configured during Argo CD installation in deploy-env.cmd
    - Eliminates need to retrieve random password each time
    - Password displayed after deployment completes

--- deploy-env.cmd
+++ deploy-env.cmd
@@ -42,7 +42,11 @@ echo [4/6] Installing Argo CD...
 kubectl create namespace argocd >nul 2>&1
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml >nul 2>&1
 timeout /t 45 /nobreak >nul
-echo [4/6] Done
+echo Setting admin password...
+kubectl -n argocd patch secret argocd-secret -p "{\"stringData\": {\"admin.password\": \"$2a$10$rRyBsGSHK6.uc8fntPwVIuLgs7mO9d.T.SrXdl9/..0vQPzQ7TL3S\", \"admin.passwordMtime\": \"$(date +%%Y-%%m-%%dT%%H:%%M:%%SZ)\"}}" >nul 2>&1
+kubectl -n argocd rollout restart deployment argocd-server >nul 2>&1
+timeout /t 15 /nobreak >nul
+echo [4/6] Done (Password: DevOps2024!)
 
 echo.
 echo [5/6] Deploying Argo CD Application...
